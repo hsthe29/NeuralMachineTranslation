ceea458386089e87a3fc9ffe1121047303a9113e
hsthe29 <The.HS200614@sis.hust.edu.vn>
2023-06-26 17:01:25 +0700

    make positional embedding

---
+++ src/model/transformer/embedding.py
@@ -0,0 +1,38 @@
+import numpy as np
+import tensorflow as tf
+
+
+def positional_encoding(length, depth):
+    pos = np.arange(length)[:, np.newaxis]
+    coeff = np.arange(depth)[np.newaxis, :] // 2
+    angle_rates = 1 / np.power(10000, (2 * coeff) / np.float32(depth))
+    angle_rads = pos * angle_rates
+
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+
+    # apply cos to odd indices in the array; 2i+1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = angle_rads[np.newaxis, ...]
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+class PositionalEmbedding(tf.keras.layers.Layer):
+    def __init__(self, vocab_size, d_model):
+        super(PositionalEmbedding, self).__init__()
+        self.d_model = d_model
+        self.embedding = tf.keras.layers.Embedding(vocab_size, d_model, mask_zero=True)
+        self.pos_encoding = positional_encoding(length=2048, depth=d_model)
+
+    def compute_mask(self, *args, **kwargs):
+        return self.embedding.compute_mask(*args, **kwargs)
+
+    def call(self, x):
+        length = tf.shape(x)[1]
+        x = self.embedding(x)
+        # This factor sets the relative scale of the embedding and positonal_encoding.
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x = x + self.pos_encoding[:, :length, :]
+        return x
